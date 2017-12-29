e858025e0d64830d0ffdc2959051d25723ec5509
ARDENS-THINKPAD\arden <achew4@jhu.edu>
2017-12-28 18:15:23 -0800

    created and compiled full board class

--- Board.java
+++ Board.java
@@ -3,4 +3,17 @@ import java.util.List;
 
 public class Board {
 	
+	private List<Column> boardList;
+
+	Board() {
+		this.boardList = new ArrayList<Column>();
+	}
+
+	public int size() {
+		return this.boardList.size();
+	}
+
+	public void insert(Column c) {
+		this.boardList.add(c);
+	}
 }
