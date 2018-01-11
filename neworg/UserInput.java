0b9bef3f33277593d8582971465196b898a303cb
ARDENS-THINKPAD\arden <achew4@jhu.edu>
2018-01-10 22:18:53 -0800

    standardize userinput

--- neworg/UserInput.java
+++ neworg/UserInput.java
@@ -6,5 +6,18 @@ public abstract class UserInput {
 	}
 
 	public INPUTTYPE type; 
+	public String data;
+
+	public UserInput(INPUTTYPE I) {
+		this.type = I;
+	}
+
+	public UserInput(INPUTTYPE I, String s) throws IllegalArgumentException {
+		if (I != INPUTTYPE.USERINPUT_GAMECOMMAND) {
+			throw new IllegalArgumentException("Illegal UserInput type.");
+		}
+		this.type = I;
+		this.data = s.toLowerCase(); //keep user input in lower case for consistency
+	}
 
 }
